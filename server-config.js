cb7d014ac436b647bfb3eafee2e4b5dff290a74e
Kent Shepard <kentshepard7@gmail.com>
2017-09-26 14:02:00 -0400

    Make a GET request to Riot's API and send a response of summoner info back to the browser

--- server-config.js
+++ server-config.js
@@ -2,6 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var Promise = require('bluebird');
 var readFile = Promise.promisify(fs.readFile);
+var api_key = require('./app/config/riot');
+var request = require('request')
 
 var app = express();
 
@@ -9,8 +11,11 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/summoner', function(req, res) {
   var summonerName = req.query.name;
-  console.log(summonerName);
-  res.end();
+  var url = `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}?api_key=${api_key}`
+  request(url, function(error, response, body) {
+    res.json(body);
+    res.end();
+  });
 });
 
 module.exports = app;
