1bd24e41efec3fe0449df14a699a5becb2af473c
Kent Shepard <kentshepard7@gmail.com>
2017-09-27 12:00:01 -0400

    Remove preset value to the search bar

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,7 +4,7 @@ class App extends React.Component {
 
     this.state = {
       matchList: [],
-      searchBar: 'ArkShepdog',
+      searchBar: '',
       accountInfo: {},
       soloQ: {},
       flexQ: {},
