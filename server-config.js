d0aff6959b3212778b16e22709219bceb0e6755e
Kent Shepard <kentshepard7@gmail.com>
2017-09-27 00:34:19 -0400

    Attempt to combine /match and /matches request to provide only necessary information to the server

--- server-config.js
+++ server-config.js
@@ -8,6 +8,7 @@ var request = require('request')
 var app = express();
 
 var searched = {};
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/summoner', function(req, res) {
@@ -53,10 +54,48 @@ app.get('/matches', function(req, res) {
   request(matchesUrl, function(error, response, body) {
     var parsedMatches = JSON.parse(body);
     var results = [];
-    for (var i = 0; i < 1; i++) {
+    for (var i = 0; i < 2; i++) {
       results.push(parsedMatches.matches[i]);
     }
-    res.send(results);
+
+    var gameStats = [];
+
+    for (let i = 0; i < results.length; i++) {
+      var gameId = results[i].gameId;
+      var champId = Number(results[i].champion);
+      var matchUrl = `https://na1.api.riotgames.com/lol/match/v3/matches/${gameId}?api_key=${api_key}`
+
+      request(matchUrl, function(error, response, body) {
+        var parsed = JSON.parse(body);
+        var players = parsed.participants;
+        var playerStats;
+        var stats = {};
+
+        var findPlayer = function(id) {
+          for (var i = 0; i < players.length; i++) {
+            if (players[i].championId === id) {
+              return players[i];
+            }
+          }
+        }
+
+        var player = findPlayer(champId)
+
+        for (var i = 0; i < parsed.teams.length; i++) {
+          if (player.teamId === parsed.teams[i].teamId) {
+            stats.result = parsed.teams[i].win;
+          }
+        }
+
+        stats.kills = player.stats.kills;
+        stats.deaths = player.stats.deaths;
+        stats.assists = player.stats.assists;
+        gameStats.push(stats);
+        console.log(gameStats);
+      });
+    }
+
+    res.send(gameStats);
   });
 });
 
