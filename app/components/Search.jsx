70fb8d87719cc4e53c497fd0d7a75267dc64e072
KentShepard <kentshepard7@gmail.com>
2018-01-09 18:31:26 -0500

    Update url on search with click and enter

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -6,21 +6,19 @@ export default class Search extends React.Component {
     super(props)
   }
 
+  handleEnterKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.props.searchName();
+      this.props.history.push(`/summoner/${this.props.searchBar}`);
+    }
+  }
+
   render() {
     return (
       <div className="search-bar form-inline row justify-content-md-center">
-        <input className="form-control search-bar" type="text" onChange={(e) => this.props.nameChange(e.target.value)} placeholder="Summoner name..." value={this.props.searchBar} onKeyPress={this.props.handleEnterKeyPress} />
+        <input className="form-control search-bar" type="text" onChange={(e) => this.props.nameChange(e.target.value)} placeholder="Summoner name..." value={this.props.searchBar} onKeyPress={this.handleEnterKeyPress.bind(this)} />
         <button className="btn btn-outline-primary" type="button" onClick={this.props.searchName}><Link to={`/summoner/${this.props.searchBar}`}>Search</Link></button>
       </div>
     )
   }
 }
-
-// var Search = ({ searchBar, searchName, nameChange, handleEnterKeyPress }) => (
-//   <div className="search-bar form-inline row justify-content-md-center">
-//     <input className="form-control search-bar" type="text" onChange={(e) => nameChange(e.target.value)} placeholder="Summoner name..." value={searchBar} onKeyPress={handleEnterKeyPress} />
-//     <button className="btn btn-outline-primary" type="button" onClick={searchName}><Link to={`/summoner/${searchBar}`}>Search</Link></button>
-//   </div>
-// );
-
-// export default Search;
